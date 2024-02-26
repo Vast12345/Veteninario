36d2a856f97c7b09be49f042005e2166dbc1fd91
Vast12345 <vastprogramming@gmail.com>
2024-02-26 15:21:43 -0500

    Base del Pagina Web.

---
+++ Firebase.js
@@ -0,0 +1,22 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA7QN_cp2Ou55RcgzoNJQEXW10ogjzUIYI",
+    authDomain: "enzovet-b8838.firebaseapp.com",
+    projectId: "enzovet-b8838",
+    storageBucket: "enzovet-b8838.appspot.com",
+    messagingSenderId: "981295256253",
+    appId: "1:981295256253:web:f7ee7b99f59d563a6c5dd4",
+    measurementId: "G-6GLFH1KWZD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
